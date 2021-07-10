5d997a502df9ac2d302af3d9523d68e919346c10
Yukwok <billynyk@gmail.com>
2021-07-10 09:22:15 +0800

    some constants added

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 # --- This is the bot to check the internet speed and post the results
 # --- to the Twitter services
+PROMISED_DOWNLOAD = 150
+PROMISED_UPLOAD = 10
+CHROME_DRIVER_PATH = ""
+TWITTER_EMAIL = ""
+TWITTER_PASSWORD = ""
