247c4de2586ef0fd7b15486861d58204714255cc
yukwok <billynyk@gmail.com>
2021-07-13 11:52:08 +0800

    class created in main

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from selenium import webdriver
+
+
 # --- This is the bot to check the internet speed and post the results
 # --- to the Twitter services
 PROMISED_DOWNLOAD = 150
@@ -5,3 +8,13 @@ PROMISED_UPLOAD = 10
 CHROME_DRIVER_PATH = ""
 TWITTER_EMAIL = ""
 TWITTER_PASSWORD = ""
+
+
+sample_element = '<span data-download-status-value="0.07" class="result-data-large number result-data-value download-speed">71.49</span>'
+
+
+class InternetSpeedTwitterBot:
+    def __init__(self, driver_path):
+        self.driver = webdriver.Chrome(executable_path=driver_path)
+        self.up = 0
+        self.down = 0
